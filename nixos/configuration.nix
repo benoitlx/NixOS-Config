7fbe0750481988f5001aa50b264899bcdc8e52bf
benoitlx <benoitleroux41@gmail.com>
2024-02-07 23:24:02 +0100

    nixos config v0

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -25,7 +25,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "rog"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
